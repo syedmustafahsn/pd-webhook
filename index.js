3d911a1580cf4bdb674643cb8cd51bfbc4934f3e
syedmustafa-m <mustafa.infoetiquette@gmail.com>
2023-06-06 23:50:38 +0500

    New commit

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const app = express();
 const http = require("http");
 const { Server } = require("socket.io");
@@ -7,13 +6,11 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.use(bodyParser.json());
-
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "https://localhost:3000",
         methods: ["GET", "POST"],
     },
 });
@@ -23,20 +20,13 @@ io.on("connection", (socket) => {
 
     socket.on("join_room", (data) => {
         socket.join(data);
-        console.log(data);
     });
 
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data);
-        console.log('send');
-    });
-
-    app.post("/webhook", (req, res) => {
-        console.log(req.body);
-        res.status(200).end();
     });
 });
 
-server.listen(3001, () => {
+server.listen(3002, () => {
     console.log("SERVER IS RUNNING");
 });
