b4e531f7e87d2e6a4477a8649d423f2552842734
syedmustafa-m <mustafa.infoetiquette@gmail.com>
2023-06-07 00:28:24 +0500

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,11 @@ io.on("connection", (socket) => {
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data);
     });
+
+    app.post("/webhook", (req, res) => {
+        console.log(req.body);
+        res.status(200).end();
+    });
 });
 
 server.listen(process.env.PORT || 3001, () => {
