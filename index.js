03c96eb7d9303151eaf7ccb01b26c7eb4082307e
syedmustafahsn <mustafa.infoetiquette@gmail.com>
2023-06-07 17:51:48 +0500

    07 final office

--- index.js
+++ index.js
@@ -1,40 +1,82 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
-const bodyParser = require("body-parser"); // Require body-parser
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const nodemailer = require('nodemailer');
+const Mux = require('@mux/mux-node');
+
+dotenv.config();
 
 app.use(cors());
-app.use(bodyParser.json()); // Use body-parser middleware
+app.use(bodyParser.json());
 
-const server = http.createServer(app);
+const { Video } = new Mux(
+    '597f62db-fd53-4eda-8a78-76a50172f379',
+    'UanvuFMUMWTR5KSm2FaR3lQxsJaIzI9FPfYk8eBfLOHtIoAsNAF0L1v6CXzdKAi8MkFzgBMNibP'
+);
 
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"],
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_SMTP_HOST,
+    port: process.env.EMAIL_SMTP_PORT,
+    auth: {
+        user: process.env.EMAIL_SMTP_USER,
+        pass: process.env.EMAIL_SMTP_PASS
     },
 });
 
-io.on("connection", (socket) => {
-    console.log(`User Connected: ${socket.id}`);
+app.post('/create-event', (req, res) => {
 
-    socket.on("join_room", (data) => {
-        socket.join(data);
-    });
+    transporter.sendMail({
+        from: process.env.EMAIL_ADDRESS_FROM,
+        to: req.body.email,
+        subject: 'Test Email',
+        text: 'This is a test email sent using Nodemailer.',
+    }, (error, info) => {
+        if (error) {
+            console.error('Error sending email:', error);
+        } else {
+            console.log('Email sent:', info.response);
+        }
+    })
 
-    socket.on("send_message", (data) => {
-        socket.to(data.room).emit("receive_message", data);
-    });
+    // Video.Spaces.create()
+    //     .then((spaceResponse) => {
+    //         const spaceID = spaceResponse.id;
 
-    app.post("/webhook", (req, res) => {
-        console.log(req.body); // Access the request body
-        socket.emit('response');
-        res.status(200).end();
-    });
+    //         Video.LiveStreams.create({
+    //             playback_policy: 'public',
+    //             new_asset_settings: { playback_policy: 'public' },
+    //         })
+    //             .then((streamResponse) => {
+    //                 const streamID = streamResponse.id;
+
+    //                 Video.Spaces.Broadcasts.create(spaceID, {
+    //                     live_stream_id: streamID,
+    //                 })
+    //                     .then((broadcastResponse) => {
+    //                         const spaceToken = Mux.JWT.signSpaceId(spaceID);
+    //                         console.log(spaceToken);
+
+    //                         res.status(200).json({ spaceToken });
+    //                     })
+    //                     .catch((error) => {
+    //                         console.error('Failed to create broadcast:', error);
+    //                         res.status(500).json({ error: 'Failed to create broadcast' });
+    //                     });
+    //             })
+    //             .catch((error) => {
+    //                 console.error('Failed to create livestream:', error);
+    //                 res.status(500).json({ error: 'Failed to create livestream' });
+    //             });
+    //     })
+    //     .catch((error) => {
+    //         console.error('Failed to create space:', error);
+    //         res.status(500).json({ error: 'Failed to create space' });
+    //     });
 });
 
-server.listen(process.env.PORT || 3001, () => {
-    console.log("SERVER IS RUNNING");
+
+app.listen(3001, () => {
+    console.log('SERVER IS RUNNING');
 });
